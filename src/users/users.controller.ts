75d21f3db669768e327d32260d7c36a16408c998
Prince Narteh <prinarteh@gmail.com>
2023-06-02 17:18:04 +0000

    add user service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
   @Get()
-  async getUsers() {
-    return ['user'];
+  async getUsers(): Promise<User[]> {
+    return this.userService.allUsers();
   }
 }
