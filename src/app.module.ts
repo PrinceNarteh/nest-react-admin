cbf77799fc46e2bbadd2e9a48664dd3e8471346e
Prince Narteh <prinarteh@gmail.com>
2023-06-02 16:59:39 +0000

    add user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'admin',
+      password: 'admin',
+      database: 'admin',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
 
 @Module({
-  controllers: [UsersController]
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
 })
 export class UsersModule {}
