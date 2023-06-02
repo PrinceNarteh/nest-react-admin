934bbcb7b9789ec639c0fdc32a357c37de5524df
Prince Narteh <prinarteh@gmail.com>
2023-06-02 17:23:53 +0000

    add validation pipe

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post()
+  async register() {
+    return 'register';
+  }
+}
