0dba09b3e2b40214b5355961e89218fb9f37212f
Prince Narteh <prinarteh@gmail.com>
2023-06-04 00:52:54 +0000

    add jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '15m' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import {
   ConflictException,
   Injectable,
+  Res,
   UnauthorizedException,
 } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
@@ -8,10 +9,15 @@ import { CreateUserDto } from 'src/users/dto/user.dto';
 import { UsersService } from 'src/users/users.service';
 import { LoginDto } from './dto/auth.dto';
 import { User } from 'src/users/entity/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async register(createUserDto: CreateUserDto): Promise<User> {
     const { password, email } = createUserDto;
@@ -29,12 +35,20 @@ export class AuthService {
     return user;
   }
 
-  async login(login: LoginDto): Promise<User> {
+  async login(
+    login: LoginDto,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<User> {
     const { email, password } = login;
     const user = await this.userService.findOne({ where: { email } });
     if (!user || !(await bcrypt.compare(password, user.password))) {
       throw new UnauthorizedException('Invalid credential');
     }
+
+    const jwt = await this.jwtService.signAsync({ id: user.id });
+
+    res.cookie('jwt', jwt, { httpOnly: true });
+
     return user;
   }
 }
